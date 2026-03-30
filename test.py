32309fb850de634ad681b9682c8b86f38a30a4c5
Michelle <1007287428@qq.com>
2026-03-30 16:15:44 +0800

    添加了test脚本

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+针对【test】的Python脚本
+@author qiaoyanhua
+@date 2026/03/30
+"""
+
+if __name__ == "__main__":
+    print("hello")
+    print("hi")
